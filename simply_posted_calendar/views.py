a0da39b059f612d23414dcf390c35341bdf9f3e6
Nadia Zaakane <nadiazaakane@gmail.com>
2017-03-02 10:43:15 +0100

    switch to fullcalendar

--- simply_posted_calendar/views.py
+++ simply_posted_calendar/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 from simply_posted_calendar.models import Publication
 
 def get_all(request):
-    publications = Publication.objects.all().values()
-    return JsonResponse({ "publications": list(publications) })
+    publications = to_dict(Publication.objects.all().exclude(approved=False))
+    return JsonResponse(publications, safe=False)
 
 def approve(request, pk):
     publication = Publication.objects.filter(pk=pk)
@@ -19,10 +19,33 @@ def reject(request, pk):
     publication = Publication.objects.filter(pk=pk)
     publication.update(approved=False)
 
-    data = publication.values().first()
-    return JsonResponse(data)
+    substituted_post = substitute(publication.first())
+    if substituted_post:
+        data = to_dict([substituted_post])
+    else:
+      data = []
+
+    return JsonResponse(data, safe=False)
 
 def get_new(request):
     user = User.objects.first()
     publications = [Publication(user=user, publication_date=datetime.datetime.now() + datetime.timedelta(days=n+1)) for n in range(10)]
     return JsonResponse({ "publications": Publication.objects.all().values() })
+
+def to_dict(publications):
+    result = []
+    for publication in publications:
+        color = '#449d44' if publication.approved else '#c9302c' if publication.approved == False else '#337ab7'
+        publication_data = {'title': 'test', 'content': 'test', 'start': publication.publication_date.isoformat(), 'id': publication.id, 'color': color}
+        result.append(publication_data)
+
+    return result
+
+def substitute(publication):
+    if publication.substituted_for and publication.substituted_for.substituted_for:
+        return False
+
+    user = publication.user
+    substitute = Publication.objects.create(publication_date=publication.publication_date, user=publication.user, substituted_for=publication)
+
+    return substitute
--- static/src/js/site.js
+++ static/src/js/site.js
@@ -4,29 +4,49 @@ window.jQuery = window.$ = require('jquery');
 const $ = window.$;
 
 require('bootstrap');
+require('fullcalendar');
 
 $(() => {
-  $('[data-toggle="popover"]').popover();
+  $('#calendar').fullCalendar({
+    events: '/publications/get_all',
+    eventClick: function(event) {
+
+      const options = {
+        title: event.title,
+        content: "<div class='date'>" + event.start.format("MMMM Do YYYY, h:mm:ss a") + "</div><div class='description'>" + event.content + "</div><div class='buttons'><button type='button' class='btn btn-primary pull-left btn-sm js-approve-content'>Approve</button><button type='button' class='btn btn-danger pull-right btn-sm js-reject-content'>Reject</button></div>",
+        html: true,
+        placement: 'top',
+        template: '<div class="popover calendar-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content" data-id="' + event.id + '"></div></div>',
+        trigger: 'manual'
+      }
+
+      $(this).popover(options);
+      $(this).popover('toggle');
+    }
+  });
+
+  const updateEvent = (e, action, callback) => {
+    const id = $(e.target).parents('.popover-content').data('id');
+
+    $.ajax({
+      method: 'POST',
+      url: '/publications/' + id + '/' + action + '/',
+      success: (data) => {
+        callback(data);
+      }
+    });
+  }
 
   $('body').on('click', '.js-approve-content', e => {
-    console.log('approve');
-    console.log($(e.target).parents('p').data('id'));
-
-    const triggerElement = $(e.target).parents('p').children('a');
-    triggerElement.popover('hide');
-
-    const td = triggerElement.parents('td');
-    td.addClass('approved');
+    updateEvent(e, 'approve', () => {
+      $('#calendar').fullCalendar('refetchEvents');
+    });
   });
 
   $('body').on('click', '.js-reject-content', e => {
-    console.log('reject');
-    console.log($(e.target).parents('p').data('id'));
-
-    const triggerElement = $(e.target).parents('p').children('a');
-    triggerElement.popover('hide');
-
-    const td = triggerElement.parents('td');
-    td.addClass('rejected');
+    updateEvent(e, 'reject', (data) => {
+      $('#calendar').fullCalendar('refetchEvents');
+      console.log(data);
+    });
   });
 });
