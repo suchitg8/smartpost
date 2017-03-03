e7cb9d2197db7b20e84c4fc3fb614a62128779d3
Nadia Zaakane <nadiazaakane@gmail.com>
2017-03-03 20:23:28 +0100

    remove old calendar and fix lint errors

--- static/src/js/site.js
+++ static/src/js/site.js
@@ -8,17 +8,19 @@ require('fullcalendar');
 
 $(() => {
   $('#calendar').fullCalendar({
+    header: {
+      left: ''
+    },
     events: '/publications/get_all',
-    eventClick: function(event) {
-
+    eventClick(event) {
       const options = {
         title: event.title,
-        content: "<div class='date'>" + event.start.format("MMMM Do YYYY, h:mm:ss a") + "</div><div class='description'>" + event.content + "</div><div class='buttons'><button type='button' class='btn btn-primary pull-left btn-sm js-approve-content'>Approve</button><button type='button' class='btn btn-danger pull-right btn-sm js-reject-content'>Reject</button></div>",
+        content: `<div class='date'>${event.start.format('MMMM Do YYYY, h:mm:ss a')}</div><div class='description'>${event.content}</div><div class='buttons'><button type='button' class='btn btn-primary pull-left btn-sm js-approve-content'>Approve</button><button type='button' class='btn btn-danger pull-right btn-sm js-reject-content'>Reject</button></div>`,
         html: true,
         placement: 'top',
-        template: '<div class="popover calendar-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content" data-id="' + event.id + '"></div></div>',
+        template: `<div class="popover calendar-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content" data-id="${event.id}"></div></div>`,
         trigger: 'manual'
-      }
+      };
 
       $(this).popover(options);
       $(this).popover('toggle');
@@ -30,12 +32,12 @@ $(() => {
 
     $.ajax({
       method: 'POST',
-      url: '/publications/' + id + '/' + action + '/',
-      success: (data) => {
+      url: `/publications/${id}/${action}/`,
+      success: data => {
         callback(data);
       }
     });
-  }
+  };
 
   $('body').on('click', '.js-approve-content', e => {
     updateEvent(e, 'approve', () => {
@@ -44,7 +46,7 @@ $(() => {
   });
 
   $('body').on('click', '.js-reject-content', e => {
-    updateEvent(e, 'reject', (data) => {
+    updateEvent(e, 'reject', data => {
       $('#calendar').fullCalendar('refetchEvents');
       console.log(data);
     });
